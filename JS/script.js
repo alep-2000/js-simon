459d4a6b7b43ec4666a8704e73c4b962ac7250e6
alep-2000 <alessiopiras056@gmail.com>
2023-05-30 19:39:21 +0200

    [FEAT LOGIC]2° sottoproblema risolto

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,10 @@
 "use strict"
 
+// FUNZIONI
+function stop(){
+    clearInterval(timer);
+}
+
 // ARRAY
 let number= [];
 
@@ -13,3 +18,13 @@ for (let i=0; i<5; i++){
 
 // RICHIAMO RANDOMNUMBER NEL DOM E MOSTRO I NUMERI RANDOM
 document.getElementById('randomNumber').innerHTML = number;
+
+// CREO VARIABILE PER IMPOSTARE I SECONDI DA CUI PARTIRE
+let seconds= 0
+// CREO VARIABILE PER GENERARE IL TIMER DI 30 SECONDI
+let timer= setInterval(function(){
+    document.getElementById('timer').innerHTML= seconds;
+    seconds++;
+}, 1000);
+
+const timeOut= setTimeout(stop, 31000);
